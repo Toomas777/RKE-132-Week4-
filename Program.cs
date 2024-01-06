c1a8a1c0cab50ed08a8ca1056ed7eda5e56d4929
Toomas J. <tom.juri@hotmail.com.>
2024-01-06 14:11:27 +0200

    add week4 code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+int i = 0; 
+for( i = 0;  i < 5; i++)
+{
+
+    Console.WriteLine("we don`t need no education.");
+
+}
+
+
+Console.WriteLine("Mõnusat päeva.");
+Console.WriteLine(i);
+//Console.WriteLine("we don`t need no education.");
